cbdeff62a9892afa5373c407c2ac6e6261e059e1
jds109 <66135437+juandiegoproch@users.noreply.github.com>
2021-03-10 11:53:22 -0500

    Uploaded Updated versions of the files

---
+++ Emulator/declarations.h
@@ -0,0 +1,56 @@
+#ifndef _DECLARATIONS
+#define _DECLARATIONS
+
+#include <stdbool.h>
+
+#define RAMSIZE 65535
+#define REGISTERNUM 8
+
+#define PC 7
+#define JR 6
+#define ML 5
+#define R2 4
+#define R1 3
+#define R0 2
+#define MD 1
+#define ZR 0
+
+// Functions
+
+// command line parsing
+void parseCliArguments(int argc,char* argv[],char** fileToRead, bool* dumpMemoryOption);
+
+// ALU operation (alu.c)
+uint16_t aluOperation(uint8_t aluConfig,uint16_t operand1,uint16_t operand2);
+
+// Register operations (registers.c)
+uint16_t registerop(int reg,char mode,uint16_t value);
+
+// Load and dump system ram,
+void memload(char* memimage_s);
+void memdump();
+
+// Instruction decoding (decode.c)
+int decode(unsigned int instruction);
+int decodeStoreReg(unsigned int instruction);
+int decodeOp1reg(unsigned int instruction);
+int decodeOp2reg(unsigned int instruction);
+
+// Instruction Execution (instructions.c)
+uint16_t fetchInstruction();
+
+void executeAluOpInstruction(uint16_t instruction);
+void executeMLoadInstruction(uint16_t instruction);
+
+// Signal Handling
+
+int userInterruptHandler(int signum);
+
+// Globals
+extern uint16_t memory[RAMSIZE];
+extern uint16_t registers[REGISTERNUM];
+
+extern bool zeroFlag;
+extern bool negativeFlag;
+
+#endif
---
+++ Emulator/main.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <signal.h>
+
+#include "declarations.h"
+
+#define ALUOP 1
+#define MLOAD 0
+
+int main(int argc,char* argv[])
+{
+	// Bind Signal handler
+	
+	  signal(SIGINT,(void*)userInterruptHandler);
+	
+	// Get CLI arguents
+	char* fileToRead = NULL;
+	bool dumpRam = false;
+	
+	parseCliArguments(argc,argv,&fileToRead, &dumpRam);
+	
+	if (fileToRead != NULL) memload(fileToRead); // Load memimg file into ram
+	
+	// Main Execution Loop
+	while(memory[RAMSIZE] == 0)
+	{
+		uint16_t currentInstruction = fetchInstruction();
+		if (decode(currentInstruction) == ALUOP)
+		{
+			executeAluOpInstruction(currentInstruction);
+		}
+		else
+		{
+			executeMLoadInstruction(currentInstruction);
+		}
+	}
+	
+	printf("Program halted with code %x \n",memory[RAMSIZE]);
+	
+	// RAM dumping
+	if (dumpRam)
+		memdump();
+		
+	
+	
+	return 0;
+}
