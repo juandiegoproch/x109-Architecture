213174600d47563cb5d8b608ca12156169630627
jds109 <juandiegopro1@gmail.com>
2023-01-23 10:09:20 -0500

    Update Registers.c

--- Emulator/Registers.c
+++ Emulator/Registers.c
@@ -1,38 +1,30 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "declarations.h"
 
 uint16_t registerop(int reg,char mode,uint16_t value)
 {
-	if (mode == 'r') // Read Operation handler
+	switch (mode)
 	{
-		if (reg>MD) // All non special behaviour regs
-		{
-			return registers[reg];
-		} 
-		else if (reg == MD) // Memory Data reg handler
-		{
-			return memory[registers[ML]]; 
-		}
-		else if (reg == ZR) // Zero register handler
+	case 'r':
+		if (reg == ZR) // Zero register handler
 		{
 			return (uint16_t)0x0000;
 		}
-	}
-	else if (mode == 'w')
-	{
-		if (reg == MD)
-		{
-			//MD storage handler
-			memory[registers[ML]] = value; 
-			registers[MD] = value; // update MD for consistency
-			return value;
-		}
-		else
-		{
-		if (reg == ML) registers[MD] = memory[value]; // Update the MD data to be consistent
-			registers[reg] = value;
-			return value;
-		}
+
+		return registers[reg];
+		break;
+	case 'w':
+		registers[reg] = value;
+		return value;
+		break;
+	
+	default:
+		printf("Invalid register access mode");
+		abort();
+		return (uint16_t) 0;
+		break;
 	}
 }
 
